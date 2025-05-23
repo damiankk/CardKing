283e43608b7f6e07db9f592af5b7c0118f718e5a
Damian Konopko <damianoryks@gmail.com>
2025-05-23 21:34:08 +1000

    Implemented foundationa areas logic

--- deck.gd
+++ deck.gd
@@ -4,6 +4,7 @@ const CardScene = preload("res://Scenes/card.tscn")
 
 var deck_cards: Array = [] # An array to hold the 52 card instances
 var tableau_areas: Array = [] # An array to hold all of TableauPileArea
+var tableau_piles: Array = [[], [], [], [], [], [], []] # An array to hold all cards within all TableauPileArea Piles - game state data - logical representation!!
 var foundation_areas: Array = [] # An array to hold all of FoundationPileArea
 var foundation_piles: Array = [[], [], [], []] # We need a way to store which cards are in each foundation pile. An array containing four other empty arrays is a good way to represent this.
 const TABLEAU_Y_OFFSET = 30
@@ -100,6 +101,7 @@ func deal_initial_tableau():
 			var dealt_card = deal_one_card(deck_cards)
 			if dealt_card:
 				tableau_area_node.add_child(dealt_card)
+				tableau_piles[i].append(dealt_card)
 				dealt_card.position = Vector2(0, j * TABLEAU_Y_OFFSET)
 				# Inside the 'if dealt_card:' block in deal_initial_tableau
 				if j == i: # Is this the last card for this pile?
@@ -183,6 +185,7 @@ func can_add_to_foundation(card_to_add: Card, foundation_index: int) -> bool:
 		else:
 			return false
 
+
 #card_node: The Card instance we want to add.
 #foundation_idx: Which foundation pile (0-3) to add it to.
 func add_card_to_foundation(card_node: Card, foundation_idx: int):
@@ -199,9 +202,7 @@ func add_card_to_foundation(card_node: Card, foundation_idx: int):
 	card_node.position = Vector2.ZERO
 	foundation_piles[foundation_idx].append(card_node)
 	
-
-
-func _on_foundation_pile_area_1_gui_input(event: InputEvent, foundation_idx: int) -> void:
+func _on_foundation_area_clicked(event: InputEvent, foundation_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
 			if not waste_cards.is_empty():
@@ -216,6 +217,100 @@ func _on_foundation_pile_area_1_gui_input(event: InputEvent, foundation_idx: int
 					
 					waste_area_node.remove_child(top_waste_card)
 					add_card_to_foundation(top_waste_card, foundation_idx)
+					
+					if not waste_cards.is_empty():
+						var new_top_waste_card : Card = waste_cards.back()
+						new_top_waste_card.visible = true
+						
+					return
+			
+			for tableau_source_idx in range(7) :
+				var tableau_pile : Array = tableau_piles[tableau_source_idx]
+				if not tableau_pile.is_empty():
+					var top_tableau_card : Card = tableau_pile.back()
+					if not top_tableau_card.is_face_up:
+						continue
+					
+					if can_add_to_foundation(top_tableau_card, foundation_idx):
+						var tableau_area_node_name = "TableauPileArea" + str(tableau_source_idx + 1)
+						print("Adding card from " + tableau_area_node_name)
+						var tableau_area_node = tableau_areas[tableau_source_idx]
+						if not tableau_area_node: 
+							print("ERROR...")
+							return
+						
+						if top_tableau_card.get_parent() != tableau_area_node:
+							print("Top tableau card parent is different than tableau area node! Escaping")
+							return
+							
+						tableau_pile.pop_back()
+						tableau_area_node.remove_child(top_tableau_card)
+						
+						add_card_to_foundation(top_tableau_card, foundation_idx)
+						
+						if not tableau_pile.is_empty():
+							flip_top_tableau_card(tableau_source_idx)
+						
+						return
+
+func flip_top_tableau_card(tableau_idx: int):
+	if tableau_piles[tableau_idx].is_empty():
+		return
+
+	var top_card : Card = tableau_piles[tableau_idx].back()
+	top_card.set_face_up(true)
+
+
+func can_add_to_tableau(card_to_move: Card, tableau_idx: int) -> bool:
+	if tableau_piles[tableau_idx].is_empty():
+		if card_to_move.rank == 13:
+			return true
+		else:
+			return false # Explicitly false if not a King on empty
+	else:
+		var top_card : Card = tableau_piles[tableau_idx].back()
+		if card_to_move.rank + 1 == top_card.rank:
+			if (card_to_move.suit in [1,2] && top_card.suit in [0,3]) || (card_to_move.suit in [0,3] && top_card.suit in [1,2]):
+				return true
+			else:
+				return false # Colors are not alternating
+		else:
+			return false # Rank is not one lower
+	return false # Should ideally not be reached if logic above is exhaustive
+
+
+func add_card_to_tableau(card_to_move: Card, tableau_idx: int):
+	# get number of cards in tableau
+	var number_of_cards = len(tableau_piles[tableau_idx])
+	
+	var tableau_area : Panel = tableau_areas[tableau_idx]
+	# add visual representation of the card
+	card_to_move.set_face_up(true)
+	card_to_move.position = Vector2(0, number_of_cards * TABLEAU_Y_OFFSET)
+	card_to_move.z_index = number_of_cards
+	
+	tableau_area.add_child(card_to_move)
+	
+	# add card to the list
+	tableau_piles[tableau_idx].append(card_to_move)
+
+
+func _on_tableau_pile_area_gui_input(event: InputEvent, tableau_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			print("Tableau " + str(tableau_idx) + " clicked")
+			if not waste_cards.is_empty():
+				var top_waste_card : Card = waste_cards.back()
+				
+				if can_add_to_tableau(top_waste_card, tableau_idx):
+					waste_cards.pop_back()
+					var waste_area_node = get_parent().get_node("WastePileArea")
+					
+					if not waste_area_node:
+						print("waste_area_node not found")
+					
+					waste_area_node.remove_child(top_waste_card)
+					add_card_to_tableau(top_waste_card, tableau_idx)
 					
 					if not waste_cards.is_empty():
 						var new_top_waste_card : Card = waste_cards.back()
